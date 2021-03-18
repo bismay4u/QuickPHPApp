94fd84e1e646ae0984e616b3df0f749148f5dfea
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2021-03-19 01:54:00 +0530

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,5 +1,15 @@
 <?php
 if(!defined('ROOT')) exit('No direct script access allowed');
 
+$_ENV['STYLES'] = [
+	"bower_components/bootstrap/dist/css/bootstrap.min.css",
 
+];
+
+$_ENV['SCRIPTS'] = [];
+$_ENV['SCRIPTS_PREBODY'] = [
+	"bower_components/jquery/dist/jquery.min.js",
+	"bower_components/bootstrap/dist/js/bootstrap.min.js"
+];
+$_ENV['SCRIPTS_POSTBODY'] = [];
 ?>
